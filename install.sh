5ef30e5e95c6405a3331d8e7d43471d3477d10ae
Hamza Ghani <h.u.f.ghani@gmail.com>
2016-09-26 23:34:49 +0100

    fix soft link

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 if [ ! -f ~/.bash_profile   ]; then
 	echo "File not found! Creating it now"
-	ls -s ~/.bash/bash_profile ~/.bash_profile
+	ln -s ~/.bash/bash_profile ~/.bash_profile
 	ls -ln ~/.bash_profile
-
 else
 	echo "remove it and creating it"
 	rm ~/.bash_profile
-	ls -s ~/.bash/bash_profile ~/.bash_profile
+	ln -s ~/.bash/bash_profile ~/.bash_profile
 	ls -ln ~/.bash_profile
+
 fi
