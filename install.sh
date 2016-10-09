a7823d8146804754a39450f3644c866eee751418
Hamza Ghani <h.u.f.ghani@gmail.com>
2016-10-09 15:18:01 +0100

    fix the install for gitconfig

--- install.sh
+++ install.sh
@@ -4,10 +4,14 @@ if [ ! -f ~/.bash_profile   ]; then
 	echo "File not found! Creating it now"
 	ln -s ~/.bash/bash_profile ~/.bash_profile
 	ls -ln ~/.bash_profile
+	ln -s ~/.bash/gitconfig ~/.gitconfig
+	ls -ln ~/.gitconfig
 else
 	echo "remove it and creating it"
 	rm ~/.bash_profile
 	ln -s ~/.bash/bash_profile ~/.bash_profile
 	ls -ln ~/.bash_profile
-
+        rm ~/.gitconfig
+	ln -s ~/.bash/gitconfig ~/.gitconfig
+	ls -ln ~/.gitconfig
 fi
